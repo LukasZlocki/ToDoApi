eaa6c03fbd120a4398288b7460d2e8d0125922a0
lzlocki <lzlocki@poczta.fm>
2023-05-18 20:06:16 +0200

    Add controllers

--- api/views.py
+++ api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Task
+from .serializers import TaskSerializer
 
 # Create your views here.
+
+class TaskList(generics.ListCreateAPIView):
+    serializer_class = TaskSerializer
+    queryset = Task.objects.all()
+    
+class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = TaskSerializer
+    queryset = Task.objects.all()
