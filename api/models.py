3c8ce4e4af89ed80109c24d12631f38cc5f66a59
lzlocki <lzlocki@poczta.fm>
2023-05-18 20:25:18 +0200

    Update model

--- api/models.py
+++ api/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class Task(models.Model):
-    title = models.CharField(max_length=100, unique=True)
-    metadata = models.CharField(max_length=50, unique=True)
+    title = models.CharField(max_length=100)
+    metadata = models.CharField(max_length=50)
     deadline = models.DateField()
-    description = models.CharField(max_length=100, unique=True)
+    description = models.CharField(max_length=300)
 
     def __str__(self):
         return self.title
