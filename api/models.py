9a11bbabd4fe0fd13f9a1ea35e997f81b666aa5b
lzlocki <lzlocki@poczta.fm>
2023-05-18 19:32:47 +0200

    Add task model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    metadata = models.CharField(max_length=50, unique=True)
+    deadline = models.DateField()
+    description = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.title
